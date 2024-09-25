967ba948006ffe91f98d006f17fb6d8aa4682968
JadFuhr <c00290965@setu.ie>
2024-09-25 15:15:13 +0100

    started question 1

--- Lab 3/Lab3/Lab3.cpp
+++ Lab 3/Lab3/Lab3.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 
-	char code[1000] = "";
+	char code[1000] = "int main() \n{\n int var1 = 10; \n int var2 = 10; \n if (var1 < 10) \n {\n int var3 = 20; \n} \n}\ n";
 
 	char variables[1000] = "";
 
--- Lab 3/Lab3/Lab3.h
+++ Lab 3/Lab3/Lab3.h
@@ -4,8 +4,21 @@
 void FindAllVariables(char variables[1000], char code[1000])
 {
 
+	for (int i = 0; i < 1000; i++)
+	{
+		if (code[i] == 'i' && code[i + 1] == 'n' && code[i + 2] == 't' && code[i + 3] != ' ') 
+		{
+			
+		}
+	}
+
 }
+
+
+
 void FindAllVariablesInScope(char variables[1000], char code[1000], int lineNumber)
 {
+
+
 
 }
