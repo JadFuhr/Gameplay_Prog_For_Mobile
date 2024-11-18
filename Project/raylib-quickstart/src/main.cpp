26947b872f26874efbd994f55bd5ddf5c50e8b3a
JadFuhr <C00290965@itcarlow.ie>
2024-11-18 23:37:14 +0000

    added starter eraser function

--- Project/raylib-quickstart/src/main.cpp
+++ Project/raylib-quickstart/src/main.cpp
@@ -17,14 +17,13 @@ int main() {
     const int screenWidth = 800;
     const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "Simple Drawing Package - Rectangle Mode");
+    InitWindow(screenWidth, screenHeight, "Simple Drawing Package - Rectangle Mode with Eraser");
 
     // Variables
     Rect rectangles[MAX_RECTS];
     int rectCount = 0;
     Color currentColor = BLACK;  // Default color for drawing rectangles
-    Vector2 rectStart = { 0 };
-    bool drawing = false;
+    bool eraserActive = false;  // Eraser tool toggle
 
     // Define a simple color palette
     Color palette[] = { BLACK, RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE, MAROON };
@@ -36,42 +35,65 @@ int main() {
     // Main game loop
     while (!WindowShouldClose()) {
         // Update
-        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-            // Start a new rectangle
-            rectStart = GetMousePosition();
-            if (rectCount < MAX_RECTS) {
-                Vector2 rectEnd = GetMousePosition();
+        Vector2 mousePosition = GetMousePosition();
 
-                rectangles[rectCount].position.x = rectStart.x;
-                rectangles[rectCount].position.y = rectStart.y;
-                rectangles[rectCount].width = 10;
-                rectangles[rectCount].height = 10;
-                rectangles[rectCount].colour = currentColor;
-                rectCount++;
+        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+            // Snap mouse position to the grid
+            float snappedX = (float)((int)mousePosition.x / GRID_SIZE) * GRID_SIZE;
+            float snappedY = (float)((int)mousePosition.y / GRID_SIZE) * GRID_SIZE;
+
+            if (eraserActive) {
+                // Eraser logic: Find and "erase" (set color to RAYWHITE)
+                for (int i = 0; i < rectCount; i++) {
+                    if (rectangles[i].position.x == snappedX && rectangles[i].position.y == snappedY) {
+                        rectangles[i].colour = RAYWHITE;
+                        break;
+                    }
+                }
+            } else {
+                // Drawing logic: Add a new rectangle
+                bool exists = false;
+                for (int i = 0; i < rectCount; i++) {
+                    if (rectangles[i].position.x == snappedX && rectangles[i].position.y == snappedY) {
+                        rectangles[i].colour = currentColor;  // Update color if it exists
+                        exists = true;
+                        break;
+                    }
+                }
+                if (!exists && rectCount < MAX_RECTS) {
+                    rectangles[rectCount].position.x = snappedX;
+                    rectangles[rectCount].position.y = snappedY;
+                    rectangles[rectCount].width = GRID_SIZE;
+                    rectangles[rectCount].height = GRID_SIZE;
+                    rectangles[rectCount].colour = currentColor;
+                    rectCount++;
+                }
             }
         }
 
-
-        // Change color based on palette selection
+        // Handle palette and eraser selection
         for (int i = 0; i < paletteSize; i++) {
             Rectangle colorButton = { 10 + 40 * i, screenHeight - 40, 30, 30 };
             if (GuiButton(colorButton, "")) {
                 currentColor = palette[i];
                 selectedColor = i;
+                eraserActive = false;  // Deactivate eraser when a color is selected
             }
         }
 
+        // Eraser button
+        Rectangle eraserButton = { 10 + 40 * paletteSize, screenHeight - 40, 60, 30 };
+        if (GuiButton(eraserButton, "Eraser")) {
+            eraserActive = true;
+        }
 
         // Draw
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        // draw grid
-
-        for (int x = 0; x < screenWidth; x += GRID_SIZE)
-        {
-            for (int y = 0; y < screenHeight; y += GRID_SIZE)
-            {
+        // Draw the grid
+        for (int x = 0; x < screenWidth; x += GRID_SIZE) {
+            for (int y = 0; y < screenHeight; y += GRID_SIZE) {
                 DrawRectangleLines(x, y, GRID_SIZE, GRID_SIZE, LIGHTGRAY);
             }
         }
@@ -80,18 +102,23 @@ int main() {
         for (int i = 0; i < paletteSize; i++) {
             Rectangle colorButton = { 10 + 40 * i, screenHeight - 40, 30, 30 };
             DrawRectangleRec(colorButton, palette[i]);
-            if (i == selectedColor) {
+            if (i == selectedColor && !eraserActive) {
                 DrawRectangleLinesEx(colorButton, 2, BLACK);  // Highlight selected color
             }
         }
 
+        // Highlight the eraser button if active
+        if (eraserActive) {
+            DrawRectangleLinesEx(eraserButton, 2, RED);  // Highlight eraser button
+        }
+
+        DrawRectangleRec(eraserButton, LIGHTGRAY);
+        DrawText("Eraser", eraserButton.x + 5, eraserButton.y + 5, 20, BLACK);
+
         // Draw all stored rectangles
         for (int i = 0; i < rectCount; i++) {
             DrawRectangleRec({ rectangles[i].position.x, rectangles[i].position.y, rectangles[i].width, rectangles[i].height }, rectangles[i].colour);
-        }
-
-        // Draw the current rectangle if drawing
-
+        }       
 
         EndDrawing();
     }
@@ -101,44 +128,3 @@ int main() {
 
     return 0;
 }
-/*#include "raylib.h"
-
-int main() {
-    // Initialization
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    InitWindow(screenWidth, screenHeight, "Draw Rectangles from Array");
-
-    // Define an array of 5 rectangles with different positions, sizes, and colors
-    Rectangle rectangles[5] = {
-        {100, 100, 120, 60},
-        {300, 150, 80, 120},
-        {500, 200, 140, 50},
-        {200, 400, 90, 90},
-        {600, 300, 110, 70}
-    };
-
-    Color colors[5] = { RED, GREEN, BLUE, YELLOW, PURPLE };  // Colors for each rectangle
-
-    SetTargetFPS(60);  // Set the frame rate
-
-    // Main game loop
-    while (!WindowShouldClose()) {
-        // Draw
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        // Loop through the array and draw each rectangle with its respective color
-        for (int i = 0; i < 5; i++) {
-            DrawRectangleRec(rectangles[i], colors[i]);
-        }
-
-        EndDrawing();
-    }
-
-    // De-Initialization
-    CloseWindow();
-
-    return 0;
-}
-*/
